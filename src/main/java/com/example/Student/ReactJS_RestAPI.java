58a2420c03eb4336850ec8cd67ac1bebafb32c75
somanaga96 <somanathan.java@gmail.com>
2025-04-01 17:07:38 +0530

    second commit

--- src/main/java/com/example/Student/ReactJS_RestAPI.java
+++ src/main/java/com/example/Student/ReactJS_RestAPI.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReactJS_RestAPI {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ReactJS_RestAPI.class, args);
 	}
 
